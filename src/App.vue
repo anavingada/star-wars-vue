18095fd11f485605176270e5519b4769678e8165
Ana Vingada <anavingada@gmail.com>
2022-05-24 11:49:42 +0100

    App.vue structure and styles
    * adding header, router view in main component and footer
    * general styles and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,60 @@
 <template>
   <div>
-    <h1>Basic Template for project</h1>
+    <the-header></the-header>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+    <the-footer></the-footer>
   </div>
 </template>
+
+<script>
+
+import TheHeader from './components/Navigation/TheHeader.vue';
+import TheFooter from './components/Navigation/TheFooter.vue';
+export default {
+  components: {
+    TheHeader, 
+    TheFooter
+  }
+};
+</script>
+
+<style>
+/* Font styles */
+* {
+  box-sizing: border-box;
+  font-family: "Work Sans Light 300", verdana, arial, sans-serif;
+}
+html {
+  font-family: sans-serif;
+}
+@font-face {
+  font-family: "Work Sans Extra-light 200";
+  src: local("Work Sans"),
+   url(./assets/fonts/Work_Sans/WorkSans-ExtraLight.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Work Sans Light 300";
+  src: local("Work Sans"),
+   url(./assets/fonts/Work_Sans/WorkSans-Light.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Work Sans Regular 400";
+  src: local("Work Sans"),
+   url(./assets/fonts/Work_Sans/WorkSans-Regular.ttf) format("truetype");
+}
+body {
+  margin: 0;
+  background-image: url('~@/assets/images/back_sky.jpg');
+}
+h1, h2 {
+  color: #ffffff;
+}
+p {
+  color:#ffffff;
+}
+img {
+  max-width: 100%;
+}
+</style>
