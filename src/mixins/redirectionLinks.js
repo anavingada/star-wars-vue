835b1323d96bed9915a670170e7c1390ed3bb037
Ana Vingada <anavingada@gmail.com>
2022-05-26 15:57:15 +0100

    RedirectionLink Mixin
    * restructuring the redirection mixin to put all methods in just one mixin

--- src/mixins/redirectionLinks.js
+++ src/mixins/redirectionLinks.js
@@ -1,50 +1,32 @@
-const pageNotFoundMixin = {
+const redirectionLinksMixin = {
     methods: {
         pageNotFound() {
             this.$router.push({
                 path: '/error-404',
                 name: 'NotFound'
             });
-        }
-    },
-};
-
-const somethingWrongMixin = {
-    methods: {
+        },
         somethingWrong() {
             this.$router.push({
                 path: '/error',
                 name: 'SomethingWrong'
             });
-        }
-    },
-};
-
-const redirectHomeMixin = {
-    methods: {
+        },
         redirectHome() {
             this.$router.push({
                 path: '/',
                 name: 'intro'
             });
-        }
-    },
-};
-
-const redirectToMoviesMixin = {
-    methods: {
+        },
         redirectToMovies() {
             this.$router.push({
                 path: '/the-sw-universe/movies',
                 name: 'theMovies'
             });
         }
-    },
+    }
 };
 
 export {
-    pageNotFoundMixin,
-    somethingWrongMixin,
-    redirectHomeMixin,
-    redirectToMoviesMixin
+    redirectionLinksMixin
 };
