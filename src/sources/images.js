c054241fb90090c54374835d2b6ba402be19251f
Ana Vingada <anavingada@gmail.com>
2022-05-31 17:14:24 +0100

    Multilanguage App - update images source

--- src/sources/images.js
+++ src/sources/images.js
@@ -1,51 +1,53 @@
+import i18n from '../localization/languageIndex';
+
 const images = [
     {
         id: '001',
-        title: 'Logo',
+        title: i18n.t("Logo"),
         link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png',
-        obs: 'From Wikimedia. Star Wars logo used in the header.'
+        obs: i18n.t("Obs_Logo")
     },
     {
         id: '002',
-        title: 'Background',
+        title: i18n.t("Background"),
         link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Night_sky_in_the_Alps_-_panoramio_%281%29.jpg/1200px-Night_sky_in_the_Alps_-_panoramio_%281%29.jpg',
-        obs: 'From Wikimedia. Background image used in all the web.'
+        obs: i18n.t("Obs_Background")
     },
     {
         id: '003',
-        title: 'Characters` poster',
+        title: i18n.t("Poster_Characters"),
         link: 'https://64.media.tumblr.com/9d934355977657e2b2c0ce0d363510f1/tumblr_p21hrfFep81v230yto1_1280.jpg',
-        obs: 'From tumblr.com. Image used in the the SW Universe page for characters reference.'
+        obs: i18n.t("Obs_Characters")
     },
     {
         id: '004',
-        title: 'Movies` poster',
+        title: i18n.t("Poster_Movies"),
         link: 'https://miro.medium.com/max/1200/0*hMSkoUZvauXqzCPl.jpg',
-        obs: 'From miro.medium.com. Image used in the the SW Universe page for movies reference.'
+        obs: i18n.t("Obs_Movies")
     },
     {
         id: '005',
-        title: 'Planets` poster',
+        title: i18n.t("Poster_Planets"),
         link: 'https://virtualbackgrounds.site/wp-content/uploads/2020/07/star-wars-the-lars-homestead-at-tatooine.jpg',
-        obs: 'From virtualbackgrounds.site. Image used in the the SW Universe page for planets reference.'
+        obs: i18n.t("Obs_Planets")
     },
     {
         id: '006',
-        title: 'Spacechips` poster',
+        title: i18n.t("Poster_Spaceships"),
         link: 'https://m.media-amazon.com/images/I/71m+UJwiabL._AC_SL1500_.jpg',
-        obs: 'From media-amazon.com. Image used in the the SW Universe page for spaceships reference.'
+        obs: i18n.t("Obs_Spaceships")
     },
     {
         id: '007',
-        title: 'Species` poster',
+        title: i18n.t("Poster_Species"),
         link: 'https://www.tvovermind.com/wp-content/uploads/2017/10/Greedo-640x360.jpg',
-        obs: 'From tvovermind.com. Image used in the the SW Universe page for species reference.'
+        obs: i18n.t("Obs_Species")
     },
     {
         id: '008',
-        title: 'Vehicles` poster',
+        title: i18n.t("Poster_Vehicles"),
         link: 'https://mvsnoticias.com/wp-content/uploads/historico/04f3d83df679fc5b6980fc091dba6428.jpg',
-        obs: 'From mvsnoticias.com. Image used in the the SW Universe page for vehicles reference.'
+        obs: i18n.t("Obs_Vehicles")
     }
 ];
 
